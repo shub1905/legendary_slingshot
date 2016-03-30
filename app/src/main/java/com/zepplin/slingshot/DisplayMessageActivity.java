175d5641a5079c390ed845faf71a48660708bb90
shub1905 <shub1905@gmail.com>
2016-03-30 01:18:16 -0400

    Twitter Integration  Next

--- app/src/main/java/com/zepplin/slingshot/DisplayMessageActivity.java
+++ app/src/main/java/com/zepplin/slingshot/DisplayMessageActivity.java
@@ -1,18 +1,21 @@
 package com.zepplin.slingshot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class displaymessageactivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_displaymessageactivity);
+        setContentView(R.layout.activity_display_message);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +28,15 @@ public class displaymessageactivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.content);
+        rl.addView(textView);
     }
 
 }
